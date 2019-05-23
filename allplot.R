770101f053faa3fe8d910d018bc45cdf49a34b58
emamian7 <38291322+emamian7@users.noreply.github.com>
2019-05-23 11:12:43 -0700

    Update allplot.R

--- allplot.R
+++ allplot.R
@@ -3,11 +3,15 @@ tail(Data1, 10)
 colnames(Data1) <- c('Date', 'Time', 'GlobalActivePower', 'GlobalReactivePower', 'Voltage',
   'GlobalIntensity', 'SubMetering1', 'SubMetering2', 'SubMetering3')
 Data1$DateTime <- strptime(paste(Data1$Date, Data1$Time), format = '%d/%m/%Y %H:%M:%S')
+
 ####Plot 1
 hist(as.numeric(Data1$GlobalActivePower), col = 'blue')
 xlab = 'Global Active Power'
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+png("plot1.png", width=480, height=480)
 ### #Plot 2
 plot(x = Data1$DateTime, y = Data1$GlobalActivePower, ylab = 'Global Active Power')
+png("plot2.png", width=480, height=480)
 ### #Plot 3
 par(mar=c(1,1,1,1))
 plot(x = Data1$DateTime, y = Data1$SubMetering1, ylab = 'Energy sub metering')
@@ -15,13 +19,14 @@ lines(x = Data1$DateTime, y = Data1$SubMetering2, col = 'blue')
 lines(x = Data1$DateTime, y = Data1$SubMetering3, col = 'red')
   legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
        col = c('black', 'blue', 'red'),
-   #dev.off()
+png("plot3.png", width=480, height=480)
+dev.off()
 ## Plot# 4
-#png('plot4.png', width = 480, height = 480)
+png('plot4.png', width = 480, height = 480)
 par(mfrow = c(2, 2)) 
-#####plot 4a = plot 1
+#####plot 4
 plot(x = Data1$DateTime, y = Data1$GlobalActivePower,  ylab = 'Global Active Power')
-########plot 4b
+########plot 4
 plot(x = Data1$DateTime, y = Data1$Voltage, xlab = 'datetime', ylab = 'Voltage')
 ###########plot 4c = plot 3
 plot(x = Data1$DateTime, y = Data1$SubMetering1, ylab = 'Energy sub metering')
@@ -29,7 +34,7 @@ lines(x = Data1$DateTime, y = Data1$SubMetering2, col = 'red')
 lines(x = Data1$DateTime, y = Data1$SubMetering3, col = 'blue')
 legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
 col = c('black', 'red', 'blue'),
-######plot 4d
+######plot 4
 plot(x = Data1$DateTime, y = Data1$GlobalReactivePower)
  xlab = 'DateTime', ylab = 'GlobalReactivePower')
-#dev.off()
+dev.off()
